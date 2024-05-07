4db8b5c95bdf542e8965429d3db1ba5476a56f7a
cryanbrow <cryan.brow@gmail.com>
2024-05-07 07:35:42 -0500

    Working on folder structure for LS and CD commands

---
+++ src/main.js
@@ -0,0 +1,135 @@
+let directoryStructure = {
+    name: "/",
+    files: [
+        {
+            attributes: '-rwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '20',
+            dt: '2024-05-05 21:13',
+            name: 'test.txt'
+        },
+        {
+            attributes: '-rwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '20',
+            dt: '2024-05-05 21:13',
+            name: 'bryan.txt'
+        }
+    ],
+    directories: [
+        {
+            attributes: 'lrwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'bin',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'boot',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'var',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'var',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'var',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'var',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'var',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'var',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'var',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'var',
+            files: [],
+            directories: []
+        },
+    ]
+}
