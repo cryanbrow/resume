90005b582637993b69f241ab23f3e797ffe0a804
cryanbrow <cryan.brow@gmail.com>
2024-05-12 10:00:51 -0500

    Building out separate files.

---
+++ src/js/directory.js
@@ -0,0 +1,179 @@
+let directoryStructure = {
+    name: "/",
+    files: [
+        {
+            attributes: '-rwxr-x-r-x',
+            executable: true,
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '20',
+            dt: '2024-05-05 21:13',
+            name: 'test.txt'
+        },
+        {
+            attributes: '-rwxr-x-r-x',
+            executable: true,
+            links: '1',
+            user: 'bryan',
+            group: 'bryan',
+            size: '20',
+            dt: '2024-05-05 21:13',
+            name: 'bryan.txt'
+        }
+    ],
+    directories: [
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '2',
+            user: 'root',
+            group: 'root',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'bin',
+            files: [
+                {
+                    attributes: '-rwxr-x-r-x',
+                    executable: true,
+                    links: '1',
+                    user: 'root',
+                    group: 'root',
+                    size: '52080',
+                    dt: '2024-05-05 21:13',
+                    name: 'cat'
+                },
+                {
+                    attributes: '-rwxr-x-r-x',
+                    executable: true,
+                    links: '1',
+                    user: 'root',
+                    group: 'root',
+                    size: '31472',
+                    dt: '2024-05-05 21:13',
+                    name: 'pwd'
+                },
+                {
+                    attributes: '-rwxr-x-r-x',
+                    executable: true,
+                    links: '1',
+                    user: 'root',
+                    group: 'root',
+                    size: '60272',
+                    dt: '2024-05-05 21:13',
+                    name: 'rm'
+                },
+                {
+                    attributes: '-rwxr-x-r-x',
+                    executable: true,
+                    links: '1',
+                    user: 'root',
+                    group: 'root',
+                    size: '64432',
+                    dt: '2024-05-05 21:13',
+                    name: 'touch'
+                },
+            ],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'root',
+            group: 'root',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'etc',
+            files: [
+                {
+                    attributes: '-r--r------',
+                    executable: false,
+                    links: '1',
+                    user: 'root',
+                    group: 'root',
+                    size: '4',
+                    dt: '2024-05-05 21:13',
+                    name: 'sudoers'
+                },
+            ],
+            directories: [
+                {
+                    attributes: 'drwxr-x-r-x',
+                    links: '1',
+                    user: 'root',
+                    group: 'root',
+                    size: '0',
+                    dt: '2024-05-05 21:13',
+                    name: 'systemd',
+                    files: [
+                        {
+                            attributes: '-rw-r--r---',
+                            executable: false,
+                            links: '1',
+                            user: 'root',
+                            group: 'root',
+                            size: '4',
+                            dt: '2024-05-05 21:13',
+                            name: 'user.conf'
+                        },
+                    ],
+                    directories: []
+                },
+            ]
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'root',
+            group: 'root',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'home',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'root',
+            group: 'root',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'opt',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'root',
+            group: 'root',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'tmp',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'root',
+            group: 'root',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'usr',
+            files: [],
+            directories: []
+        },
+        {
+            attributes: 'drwxr-x-r-x',
+            links: '1',
+            user: 'root',
+            group: 'root',
+            size: '0',
+            dt: '2024-05-05 21:13',
+            name: 'var',
+            files: [],
+            directories: []
+        },
+    ]
+}
---
+++ src/js/resume.js
@@ -0,0 +1,235 @@
+let resumeJson = {
+    "contact": {
+        "firstName": "Bryan",
+        "middleName": "Matthew",
+        "lastName": "Crow",
+        "linkedinUrl": "linkedin.com/in/bryan-crow",
+        "githubUrl": "github.com/cryanbrow",
+        "websiteUrl": "bryan-crow.com",
+        "phoneNumber": "870-754-2665",
+        "email": "cryan.brow@gmail.com"
+    },
+    "education" : [],
+    "certifications": [
+        {
+            "name": "Certified Kubernetes Application Developer (CKAD)",
+            "issuer": "Cloud Native Computing Foundation (CNCF)",
+            "issueYear": "2023",
+            "issueMonth": "Jan",
+            "expires": true,
+            "expirationYear": "2026",
+            "expirationMonth": "Jan"
+        },
+        {
+            "name": "Professional To Leader Graduate",
+            "issuer": "FedEx",
+            "issueYear": "2018",
+            "issueMonth": "Nov",
+            "expires": false,
+            "expirationYear": "",
+            "expirationMonth": ""
+        },
+        {
+            "name": "Dale Carnegie Course",
+            "issuer": "Dale Carnegie & Associates Inc",
+            "issueYear": "2015",
+            "issueMonth": "Nov",
+            "expires": false,
+            "expirationYear": "",
+            "expirationMonth": ""
+        },
+    ],
+    "awards": [
+        {
+            "issuer": "FedEx",
+            "name": "Five Star Award",
+            "awardYear": "2018",
+            "awardMonth": "Aug",
+            "description": "The Five Star Award is the highest honor at FedEx. It recognizes team members who have enhanced service and profitability and exemplified the spirit of teamwork. This award is given annually, and managers nominate their team members for this prestigious recognition.",
+            "recognition": "Awarded for adoption of Cassandra, Kafka, Spring Boot, and other technologies, paving the way for RFID."
+        },{
+            "issuer": "FedEx",
+            "name": "Global Opco Technologies Excellence Award",
+            "awardYear": "2021",
+            "awardMonth": "Jun",
+            "description": "Award recognizing team members in aid of unifying of FedEx Operating Companies",
+            "recognition": "Awarded for PKS adoption for the RFID edge stack and construction of pipelines to support adoption of Kubernetes."
+        },
+        {
+            "issuer": "FedEx",
+            "name": "Hanschu Luminary Award",
+            "awardYear": "2023",
+            "awardMonth": "May",
+            "description": "",
+            "recognition": ""
+        }
+    ],
+    "skills": [
+        {
+            "name": "container_orchestration",
+            "skillLevel": "expert",
+            "highlight": false,
+            "subSkills": [
+                {
+                    "name": "kubernetes",
+                    "skillLevel": "expert",
+                    "highlight": false,
+                    "subSkills": [
+                        {
+                            "name": "PKS/TKGI/TKGS/TKG2",
+                            "skillLevel": "expert",
+                            "highlight": false,
+                            "subSkills": []
+                        },
+                        {
+                            "name": "AKS/GKE",
+                            "skillLevel": "expert",
+                            "highlight": false,
+                            "subSkills": []
+                        },
+                        {
+                            "name": "k3s/microK8s/minikube",
+                            "skillLevel": "expert",
+                            "highlight": false,
+                            "subSkills": []
+                        },
+                        {
+                            "name": "helm/kustomize",
+                            "skillLevel": "expert",
+                            "highlight": false,
+                            "subSkills": []
+                        },
+                    ]
+                },
+                {
+                    "name": "container_distribution",
+                    "skillLevel": "expert",
+                    "highlight": false,
+                    "subSkills": [
+                        {
+                            "name": "docker",
+                            "skillLevel": "expert",
+                            "highlight": false,
+                            "subSkills": []
+                        },
+                        {
+                            "name": "podman",
+                            "skillLevel": "expert",
+                            "highlight": false,
+                            "subSkills": []
+                        },
+                        {
+                            "name": "jib",
+                            "skillLevel": "expert",
+                            "highlight": false,
+                            "subSkills": []
+                        },
+                        {
+                            "name": "harbor",
+                            "skillLevel": "expert",
+                            "highlight": false,
+                            "subSkills": []
+                        },
+                    ]
+                },
+            ]
+        },
+    ],
+    "introduction": "Lead Technical Developer with greater than ten years of leadership experience in process and technical change. Experienced in training and advancing teams in adoption of modern coding, design, and pairing practices. Successfully implemented large scale, data intensive, IOT implementations for large automation projects.",
+    "keywords": [
+        "Software Developer",
+        "Software Architect",
+        "IOT Developer",
+        "Data Design"
+    ],
+    "positions": [
+        {
+            "employer": "FedEx",
+            "title": "Chief Engineering Specialist Principal",
+            "startYear": "2022",
+            "startMonth": "09",
+            "endYear": "",
+            "endMonth": "",
+            "activeRole": true,
+            "achievements": [
+                "Rolled out dimensioning in motion software to 150+ service centers",
+                "Resulted in 25 million dollars in recovered revenue per quarter",
+                "Filed 8 patents surrounding real time location tracking with RFID"
+            ],
+            "description": "Led the rollout of dimensioning in motion for increased revenue recovery by FedEx Freight. Completed proof of concept for migrating far edge technologies to the near edge. Broadened oversight to help a wider array of teams beyond immediate team in retiring legacy systems, hardening of critical business systems, and migration of a FedEx owned data center to a managed data center."
+        },
+        {
+            "employer": "FedEx",
+            "title": "Software Developer Advisor",
+            "startYear": "2018",
+            "startMonth": "12",
+            "endYear": "2022",
+            "endMonth": "08",
+            "activeRole": false,
+            "achievements": [
+                "Implemented anedge security solution with trenching capabilities",
+                "Pioneered Kubernetes adoption in production for FedEx",
+                "Architecture guidance for the rewriting the forklift computer"
+            ],
+            "description": "Led a team in adoption of technologies and practices around Kubernetes. Implemented proofs of concepts to determine viability of new technologies such as Kubernetes, Helm, Docker, Prometheus, Alert Manager, Grafana, and other technologies for supporting a far edge ecosystem. Continued refinement of the edge ecosystem, and the first adoption of OKTA OAuth2 at Freight."
+        },
+        {
+            "employer": "FedEx",
+            "title": "Senior Programmer Analyst",
+            "startYear": "2016",
+            "startMonth": "08",
+            "endYear": "2018",
+            "endMonth": "12",
+            "activeRole": false,
+            "achievements": [
+                "Established Kafka and Cassandra implementation at FedEx",
+                "Created visualizer to display location of shipments and forklifts"
+            ],
+            "description": "Spearheaded adoption of Cassandra, Kafka, Spring Boot, and microservice architecture in building the RFID stack. Built and led workshops for onboarding new team members onto the RFID stack and new technologies for rapid adoption. Built technologies for tracking RFID tags in real time using a distributed edge compute model."
+        },
+        {
+            "employer": "FedEx",
+            "title": "Programmer Analyst",
+            "startYear": "2013",
+            "startMonth": "11",
+            "endYear": "2018",
+            "endMonth": "08",
+            "activeRole": false,
+            "achievements": [],
+            "description": "Advocated for and implemented the first REST services at FedEx Freight and the first single page Angular application at Freight. Allowing for adoption of Agile and microservice technologies."
+        },
+        {
+            "employer": "FedEx",
+            "title": "Associate Programmer Analyst",
+            "startYear": "2012",
+            "startMonth": "06",
+            "endYear": "2013",
+            "endMonth": "11",
+            "activeRole": false,
+            "achievements": [],
+            "description": "Built software for syncing and reconciling Freight customer data and to reduce data inconsistency and improve customer experience between Freight and Enterprise customer systems."
+        },
+        {
+            "employer": "FedEx",
+            "title": "Lead Computer Operator",
+            "startYear": "2011",
+            "startMonth": "05",
+            "endYear": "2012",
+            "endMonth": "06",
+            "activeRole": false,
+            "achievements": [],
+            "description": "Shift lead responsible for activities within a data center including production installs, monitoring, alerting, and resolving issues. Built Java software for tracking, recovery, and long storage of offsite backup media."
+        },
+        {
+            "employer": "FedEx",
+            "title": "Software Developer Advisor",
+            "startYear": "2007",
+            "startMonth": "04",
+            "endYear": "2011",
+            "endMonth": "05",
+            "activeRole": false,
+            "achievements": [],
+            "description": "Responsible for monitoring, alerting, and resolving issues with servers, networking equipment, mainframe processes, and other data center centric activites."
+        }
+    ]
+}
